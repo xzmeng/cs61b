1cc639f0880fb7a22ca7bf65963bdd36a56115de
xzmeng <15195891330@163.com>
2020-03-05 21:14:12 +0800

    completed lab1.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,14 @@ public class LeapYear {
         }
     }
 
+	/** Judge if a year is leap year.
+	 *  @param year to be analyzed
+	 */
+	private static boolean isLeapYear(int year) {
+		if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) return true;
+		else return false;
+	}
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
