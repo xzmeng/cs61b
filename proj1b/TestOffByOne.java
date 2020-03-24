81bf0d08f9983eaff0bb8a32676c900aadaca3e7
Meng Xiangzhuo <aumo@foxmail.com>
2020-03-24 21:49:51 +0800

    proj1b: Add test for letter case and non-letter chars.

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,17 +9,26 @@ public class TestOffByOne {
     // Your tests go here.
     @Test
     public void testEqualChars() {
-        char a = 'a';
-        char b = 'b';
-        assertTrue(offByOne.equalChars(a, b));
-
-        a = 'a';
-        b = 'a';
-        assertFalse(offByOne.equalChars(a, b));
-
-        a = 'a';
-        b = 'e';
-        assertFalse(offByOne.equalChars(a, b));
-    }
+        assertTrue(offByOne.equalChars('x', 'y'));
+        assertTrue(offByOne.equalChars('z', 'y'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('b', 'a'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('B', 'A'));
+        assertTrue(offByOne.equalChars('1', '2'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertTrue(offByOne.equalChars('@', 'A'));
+        assertTrue(offByOne.equalChars('`', 'a'));
+        assertTrue(offByOne.equalChars('z', '{'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('A', 'C'));
+        assertFalse(offByOne.equalChars('c', '3'));
+        assertFalse(offByOne.equalChars('x', 'z'));
+        assertFalse(offByOne.equalChars('Z', 'X'));
+        assertFalse(offByOne.equalChars('1', '3'));
+        assertFalse(offByOne.equalChars('?', '!'));
+        assertFalse(offByOne.equalChars('`', 'A'));
+        assertFalse(offByOne.equalChars(' ', '"'));    }
 
 }
